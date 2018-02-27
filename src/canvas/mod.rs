f6c6e1e1bb2ebe6f0eceb20c31a5315fce03c7eb
yorkyao <yao.yao@arkie.ai>
2018-02-27 16:12:19 +0800

    feat: export context_2d to use `ToAzureStyle`

--- src/canvas/mod.rs
+++ src/canvas/mod.rs
@@ -6,6 +6,7 @@ mod paintstate;
 pub use self::canvas_element::{CanvasElement, CanvasContextType};
 pub use self::paintstate::*;
 pub use self::canvas_trait::*;
+pub use self::context_2d::*;
 
 pub fn create_canvas(width: i32, height: i32, ctx_type: CanvasContextType) -> CanvasElement {
   CanvasElement::new(width, height, ctx_type).unwrap()
