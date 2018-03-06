a203c9de16f541f2fb3fb859828b41e439dbdc95
LongYinan <lynweklm@gmail.com>
2018-03-06 14:27:04 +0800

    fix: build in linux

--- src/canvas/get_target_glx.rs
+++ src/canvas/get_target_glx.rs
@@ -1,5 +1,6 @@
 use azure::azure_hl::{BackendType, DrawTarget, SurfaceFormat};
+use euclid::{Size2D};
 
 pub fn get_draw_target(size: Size2D<i32>) -> DrawTarget {
-  DrawTraget::new(BackendType::Skia, size, SurfaceFormat::B8G8R8A8)
+  DrawTarget::new(BackendType::Skia, size, SurfaceFormat::B8G8R8A8)
 }
